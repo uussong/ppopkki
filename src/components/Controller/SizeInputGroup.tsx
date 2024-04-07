8ae6c2ef67e386e52f1979f2944b933368e58cdf
uussong <77879633+uussong@users.noreply.github.com>
2024-04-07 22:30:05 +0900

    refactor: SizeInputGroup에 react-hook-form 적용

--- src/components/Controller/SizeInputGroup.tsx
+++ src/components/Controller/SizeInputGroup.tsx
@@ -1,26 +1,58 @@
-import { ChangeEvent } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { A4 } from '../../constants/paper'
 
 interface SizeInputGroupProps {
   setWidth: (width: number) => void
   setHeight: (width: number) => void
 }
 
-function SizeInputGroup({ setWidth, setHeight }: SizeInputGroupProps) {
-  const handleWidth = (e: ChangeEvent<HTMLInputElement>) => {
-    const width = parseInt(e.target.value) || 0
-    setWidth(width)
-  }
+interface FormInput {
+  width: number
+  height: number
+}
 
-  const handleHeight = (e: ChangeEvent<HTMLInputElement>) => {
-    const height = parseInt(e.target.value) || 0
+function SizeInputGroup({ setWidth, setHeight }: SizeInputGroupProps) {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormInput>()
+
+  const onSubmit: SubmitHandler<FormInput> = ({ width, height }) => {
+    setWidth(width)
     setHeight(height)
   }
 
   return (
-    <>
-      <input type="number" placeholder="너비" onChange={handleWidth} />
-      <input type="number" placeholder="높이" onChange={handleHeight} />
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="number"
+        placeholder="너비"
+        {...register('width', {
+          required: true,
+          min: 1,
+          max: A4.WIDTH - A4.PADDING * 2,
+        })}
+      />
+      {errors.width?.type === 'required' && <p>너비를 입력해주세요</p>}
+      {errors.width?.type === 'max' && (
+        <p>최대 너비는 {A4.WIDTH - A4.PADDING * 2}mm입니다</p>
+      )}
+      <input
+        type="number"
+        placeholder="높이"
+        {...register('height', {
+          required: true,
+          min: 1,
+          max: A4.HEIGHT - A4.PADDING * 2,
+        })}
+      />
+      {errors.height?.type === 'required' && <p>너비를 입력해주세요</p>}
+      {errors.height?.type === 'max' && (
+        <p>최대 너비는 {A4.HEIGHT - A4.PADDING * 2}mm입니다</p>
+      )}
+      <input type="submit" />
+    </form>
   )
 }
 
