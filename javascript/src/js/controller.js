8791043ab63f4528f6f6ae0ae5c765e556cc18b7
uussong <77879633+uussong@users.noreply.github.com>
2024-07-20 01:37:12 +0900

    feat: 설정한 최솟값, 최댓값 적용

--- javascript/src/js/controller.js
+++ javascript/src/js/controller.js
@@ -1,11 +1,19 @@
 import { A4 } from '../constants/paper.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const widthPlaceholder = `10~${A4.WIDTH - A4.PADDING * 2} (mm)`
-  const heightPlaceholder = `10~${A4.HEIGHT - A4.PADDING * 2} (mm)`
+  const minValue = 10
+  const maxWidth = A4.WIDTH - A4.PADDING * 2
+  const maxHeight = A4.HEIGHT - A4.PADDING * 2
 
-  document.getElementById('width').setAttribute('placeholder', widthPlaceholder)
+  document
+    .getElementById('width')
+    .setAttribute('placeholder', `10~${maxWidth} (mm)`)
+  document.getElementById('width').setAttribute('min', minValue)
+  document.getElementById('width').setAttribute('max', maxWidth)
+
   document
     .getElementById('height')
-    .setAttribute('placeholder', heightPlaceholder)
+    .setAttribute('placeholder', `10~${maxHeight} (mm)`)
+  document.getElementById('height').setAttribute('min', minValue)
+  document.getElementById('height').setAttribute('max', maxHeight)
 })
